603494b1fff2a6c3b103ce3e66cb790684f0a454
Sause <lazureys@gmail.com>
2016-01-11 09:25:51 +0800

    add coop landing page and faq page

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,4 @@
 (function($) {
-
   
   function init_tabs() {
     $('.tab a').click(function() {
@@ -13,9 +12,22 @@
     })
   }
 
+  function init_nav() {
+    var page = $('body').attr('data-page') || 'index';
+    $('.nav li[data-page="' + page + '"]').addClass('active');
+  }
+
+  function init_faq() {
+    $('.faq-list li h4').click(function() {
+      $(this).next().slideToggle();
+    });
+  }
+
   $(document).ready(function() {
     console.log('initializing');
+    init_nav();
     init_tabs();
+    init_faq();
   });
 
 })(jQuery || window.jQuery);
